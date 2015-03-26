cef43894ff6742186f9770957779c469200684fa
Andrew Neely <andrew.neely@agapered.com>
2015-03-25 20:38:08 -0500

    adding tests for TopicsController#index

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 RSpec.describe TopicsController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
+  before{ get :index }
+  describe 'GET index' do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
-  end
 
+    context 'gets a list of the topics' do
+      describe 'and when there are none' do
+        it 'the list is empty' do
+          expect(assigns(:topics)).to eq([])
+        end
+      end
+
+      describe 'and when there are some' do
+        it 'the list is comprehensive' do
+          topic = Topic.create(title:'foo')
+          expect(assigns(:topics)).to eq([topic])
+        end
+      end
+    end
+  end
 end
